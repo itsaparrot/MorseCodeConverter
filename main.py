27a4e1a5d7ea68e6e879218e754fb6d1642e765a
Slowbro <squirrelsnake34@gmail.com>
2021-11-09 19:47:19 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+letters_dict = {' ': '/', 'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.',
+                'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---',
+                'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--',
+                'x': '-..-', 'y': '-.--', 'z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+                '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
+
+
+# translated input letters to morse code
+def letters_to_morse():
+    user_words = input('Type words to be translated to morse: ').lower()
+    translated_word = []
+    valid_characters = True
+    for character in user_words:
+        if character in letters_dict:
+            translated_word.append(letters_dict[character])
+        else:
+            valid_characters = False
+    if valid_characters:
+        print(' '.join(translated_word))
+    else:
+        print('Invalid characters. Valid characters include a-z, and 0-9.')
+
+
+# translated user input morse to letters
+def morse_to_letters():
+    user_morse = input('Type the morse code using "." and "-" symbols: ').lower()
+    user_morse_list = user_morse.split()
+    translated_morse = []
+    valid_morse = True
+    # inverts dictionary key value pairs, keys are now morse, values are letters
+    morse_dict = {morse: letter for letter, morse in letters_dict.items()}
+    for character in user_morse_list:
+        if character in morse_dict:
+            translated_morse.append(morse_dict[character])
+        #     morse code can have '/' to space out words
+        elif character == '/':
+            pass
+        else:
+            valid_morse = False
+    if valid_morse:
+        print(' '.join(translated_morse))
+    else:
+        print('Invalid morse characters.')
+
+
+user_response = True
+while user_response:
+    valid_response = True
+    while valid_response:
+        answer = input('Type "text" if you want to translate text to morse, or type "morse" if you want to translate '
+                       'morse to text: ').lower()
+        if answer == 'text':
+            letters_to_morse()
+        elif answer == 'morse':
+            morse_to_letters()
+        valid_response = False
+
+    continue_question = input('Would you like to keep translating? "y" or "n" ')
+    if continue_question == 'n':
+        user_response = False
+
